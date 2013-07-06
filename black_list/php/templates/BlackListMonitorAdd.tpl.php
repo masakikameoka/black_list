642578651e59fe6220d961dac0094baeba571ec4
kameokamasaki <kameokamasaki@192.168.3.2>
2013-07-06 04:25:36 -0700

    テンプレート作成

--- black_list/php/templates/BlackListMonitorAdd.tpl.php
+++ black_list/php/templates/BlackListMonitorAdd.tpl.php
@@ -1,1 +1,36 @@
-<?php
+<html>
+
+<head>
+<title>ブラックリスト登録・削除画面</title>
+</head>
+
+<body>
+<h2>ブラックリストモニター登録</h2>
+
+<form action="/php/action/BlackListMonitorAddAction.class.php" method="post"
+enctype="multipart/form-data">
+
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file" />
+<br />
+<input type="submit" name="submit" value="登録する" />
+</form>
+<br />
+
+<h2>ブラックリストモニター解除</h2>
+<form action="upload_file.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file" />
+<br />
+<input type="submit" name="submit" value="登録する" />
+</form>
+
+<br>
+
+<h2>全件ダウンロード</h2>
+<form action="" method="post">
+<input type="submit" name="submit" value="全件ダウンロード">
+</form>
+</body>
+</html>
